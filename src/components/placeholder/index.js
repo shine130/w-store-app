8fe028b7a9905a0e0402ea04730a0ac5d6c7827d
shine <shine130@vip.qq.com>
2019-03-21 10:53:17 +0800

    产品列表占位符完成

---
+++ src/components/placeholder/index.js
@@ -0,0 +1,37 @@
+import Taro,{ Component } from '@tarojs/taro'
+import { View } from '@tarojs/components'
+
+class Placeholder extends Component {
+  static options = {
+    addGlobalClass: true
+  }
+
+  static defaultProps = {
+    quantity:1,
+    show:false
+  }
+
+  render (){
+    const quantity = parseInt(this.props.quantity)
+    const items = [...Array(quantity).keys()]
+
+    const { show } = this.props
+
+    return (
+      <View>
+        {
+          show &&
+          items.map(i => 
+            <View key={i} className="ui placeholder">
+              <View className="image rectangular"></View>
+              <View className="line"></View>
+              <View className="very short line"></View>
+            </View>
+            )
+        }
+      </View>
+    )
+  }
+}
+
+export default Placeholder
--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -2,6 +2,7 @@ import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import SearchBar from '../../components/search-bar'
 import ProductList from '../../components/product-list'
+import Placeholder from '../../components/placeholder'
 
 class ShopIndex extends Component {
   config = {
@@ -9,7 +10,8 @@ class ShopIndex extends Component {
   }
 
   state = {
-    products:[]
+    products:[],
+    placeholder:true
   }
 
   async componentWillMount(){
@@ -18,17 +20,19 @@ class ShopIndex extends Component {
     })
 
     this.setState({
-      products:response.data
+      products:response.data,
+      placeholder:false
     })
 
   }
 
   render() {
-    const { products } = this.state
+    const { products ,placeholder } = this.state
 
     return (
       <View>
         <SearchBar />
+        <Placeholder quantity='10' show={placeholder} />
         <ProductList data={products} />
       </View>
     )
